1c97d01e9b43b1f7ed6a50df071b9ca3f2798502
Chanmi An <bangoul345@gmail.com>
2023-09-20 16:34:35 +0900

    회원가입 완료 페이지 controller 등록

--- write_project/src/main/java/controller/RegisterController.java
+++ write_project/src/main/java/controller/RegisterController.java
@@ -2,15 +2,36 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import member.*;
 
 @Controller
 @RequestMapping("/register")
 public class RegisterController {
+	
+	@Autowired
+	private MemberRegisterService memberRegisterService;
+	
+	public void setMemberRegisterService(MemberRegisterService memberRegisterService) {
+		this.memberRegisterService = memberRegisterService;
+	}
 
 	@GetMapping("/registerPage")
 	public String handleRegister() {
 		return"register/registerPage";
+	}
+	
+	@PostMapping("/registerComplete")
+	public String handleComplete(RegisterRequest regReq) {
+		try {
+			memberRegisterService.regist(regReq);
+			return "register/registerComplete";
+		} catch(DuplicateMemberException ex) {
+			return "register/registerPage";
+		}
+		
 	}
 }
