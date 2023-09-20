4ea1e29a060fa5b325f256cb833a34d91a8395d9
Chanmi An <bangoul345@gmail.com>
2023-09-20 14:45:58 +0900

    회원 가입 정보 객체

---
+++ write_project/src/main/java/member/RegisterRequest.java
@@ -0,0 +1,60 @@
+package member;
+
+public class RegisterRequest {
+
+	private String nickname;
+	private String id;
+	private String password;
+	private String confirmPassword;
+	private String name;
+	private String email;
+	
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
+	public String getNickname() {
+		return nickname;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+	
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+}
+
