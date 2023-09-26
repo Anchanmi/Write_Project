4499a4a42574864cff34f1107e5e99d4ed28d3fc
Chanmi An <bangoul345@gmail.com>
2023-09-26 17:01:56 +0900

    세션 설정

--- write_project/src/main/java/controller/LoginController.java
+++ write_project/src/main/java/controller/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
+import javax.servlet.http.HttpSession;
 
 import member.*;
 
@@ -26,13 +27,14 @@ public class LoginController {
 	}
 	
 	@PostMapping
-	public String submit(LoginCommand loginCommand, Errors errors) {
+	public String submit(LoginCommand loginCommand, Errors errors, HttpSession session) {
 		new LoginCommandValidator().validate(loginCommand, errors);
 		if(errors.hasErrors()) {
 			return "login/loginForm";
 		}
 		try {
 			AuthInfo authInfo = authService.authenticate(loginCommand.getId(), loginCommand.getPassword());
+			session.setAttribute("authInfo", authInfo);
 			return "login/loginSuccess";
 		}catch(WrongIdPasswordException e) {
 			errors.reject("passwordNotMatching");
