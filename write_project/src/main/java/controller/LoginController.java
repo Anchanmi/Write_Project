ad2e7400576d622cc80d0792f467badb1bd3113c
Chanmi An <bangoul345@gmail.com>
2023-09-26 16:48:18 +0900

    Authwired 추가

--- write_project/src/main/java/controller/LoginController.java
+++ write_project/src/main/java/controller/LoginController.java
@@ -5,6 +5,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import member.*;
 
@@ -12,6 +13,7 @@ import member.*;
 @RequestMapping("/login")
 public class LoginController {
 
+	@Autowired
 	private AuthService authService;
 	
 	public void setAuthService(AuthService authService) {
