a3833fe408bee4ce1fefd7141f14abb3afe3b7df
Chanmi An <bangoul345@gmail.com>
2023-09-26 16:04:43 +0900

    주석처리

--- write_project/src/main/java/member/DuplicateMemberException.java
+++ write_project/src/main/java/member/DuplicateMemberException.java
@@ -1,7 +1,7 @@
 package member;
 
 public class DuplicateMemberException extends RuntimeException{
-	
+	//멤버가 존재할 때 발생하는 에러
 	public DuplicateMemberException(String message) {
 		super(message);
 	}
