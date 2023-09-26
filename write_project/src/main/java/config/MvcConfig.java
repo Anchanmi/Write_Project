7eb2fcf2524d95c4e1af376dc38df27bdb89fd48
Chanmi An <bangoul345@gmail.com>
2023-09-26 15:16:22 +0900

    main View 설정 추가

--- write_project/src/main/java/config/MvcConfig.java
+++ write_project/src/main/java/config/MvcConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.config.annotation.DefaultServletHandlerCo
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.support.ResourceBundleMessageSource;
@@ -22,6 +23,11 @@ public class MvcConfig implements WebMvcConfigurer{
 	@Override
 	public void configureViewResolvers(ViewResolverRegistry registry) {
 		registry.jsp("/WEB-INF/view/", ".jsp");
+	}
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/main").setViewName("main");
 	}
 	
 	@Bean
