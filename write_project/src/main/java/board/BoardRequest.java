88aea2f57a0f798d8c07e7e4f50feb87cd8875c3
Chanmi An <bangoul345@gmail.com>
2023-09-27 16:11:19 +0900

    글 등록 정보 객체

---
+++ write_project/src/main/java/board/BoardRequest.java
@@ -0,0 +1,23 @@
+package board;
+
+public class BoardRequest {
+
+	private String subject;
+	private String content;
+	
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	
+	public String getSubject() {
+		return subject;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+}
