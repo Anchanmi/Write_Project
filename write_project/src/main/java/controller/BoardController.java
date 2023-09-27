222ecd6a097e77719217911d0dfda6706509b6bc
Chanmi An <bangoul345@gmail.com>
2023-09-27 17:47:51 +0900

    board controller 구현

---
+++ write_project/src/main/java/controller/BoardController.java
@@ -0,0 +1,45 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import javax.validation.Valid;
+
+import board.*;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+
+	@Autowired
+	private BoardService boardService;
+	
+	public void setBoardService(BoardService boardService) {
+		this.boardService = boardService;
+	}
+	
+	@GetMapping("/boardForm")
+	public String handleBoardRegister(Model model) {
+		model.addAttribute("boardRequest", new BoardRequest());
+		return "board/boardForm";
+	}
+	
+	@PostMapping("/boardSuccess")
+	public String hadleBoardSuccess(BoardRequest br, Errors errors) {
+		new BoardRequestValidator().validate(br, errors);
+		if(errors.hasErrors()) {
+			return "board/boardForm";
+		}
+		try {
+			boardService.regist(br);
+			return "board/boardSuccess";
+		} catch(NoAuthInfoException en) {
+			errors.reject("NoAuthInfo");
+			return "redirect:/login/loginForm";
+		}
+	}
+}
