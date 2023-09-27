ef71579322820390d0038ee5ec34959b5988d8bf
Chanmi An <bangoul345@gmail.com>
2023-09-27 19:16:41 +0900

    regist 메소드 수정

--- write_project/src/main/java/board/BoardService.java
+++ write_project/src/main/java/board/BoardService.java
@@ -1,26 +1,18 @@
 package board;
 
 import java.time.LocalDateTime;
-import member.AuthInfo;
-import javax.servlet.http.HttpSession;
 
 public class BoardService {
 	
 	private BoardDao boardDao;
-	private HttpSession session;
 	
 	public BoardService(BoardDao boardDao) {
 		this.boardDao = boardDao;
 	}
 	
 	public void regist(BoardRequest br) {
-		AuthInfo authInfo = (AuthInfo) session.getAttribute("authInfo");
 		
-		if(authInfo == null) {
-			throw new NoAuthInfoException();
-		}
-		
-		Board newBoard = new Board(br.getSubject(), br.getContent(), authInfo.getNickname(),
+		Board newBoard = new Board(br.getSubject(), br.getContent(), br.getNickname(),
 									0, LocalDateTime.now());
 		boardDao.insert(newBoard);
 		
