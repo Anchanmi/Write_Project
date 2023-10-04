9eea5497bec2c4fb2e2773b8cf3aea110bc4df65
Chanmi An <bangoul345@gmail.com>
2023-10-04 17:23:02 +0900

    글 내용 볼 수 있도록 메인 컨트롤러 메소드 추가

--- write_project/src/main/java/controller/MainController.java
+++ write_project/src/main/java/controller/MainController.java
@@ -2,6 +2,8 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 
@@ -24,6 +26,13 @@ public class MainController {
 		List<BoardList> boardList = boardService.list();
 		model.addAttribute("boardList", boardList);
 		return "main";
+	}
+	
+	@GetMapping("/main/{id}") //글 내용 보기
+	public String content(@PathVariable("id") int id, Model model) {
+		Board board = boardService.content(id);
+		model.addAttribute("board", board);
+		return "main/content";
 	}
 
 }
