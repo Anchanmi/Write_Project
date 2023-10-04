a486e500ed9e4901876e94c309806b40e9eaf1dc
Chanmi An <bangoul345@gmail.com>
2023-10-04 16:23:40 +0900

    메인페이지 컨트롤러 분리

--- write_project/src/main/java/controller/MainController.java
+++ write_project/src/main/java/controller/MainController.java
@@ -2,11 +2,27 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+
+import java.util.List;
+
+import board.*;
 
 @Controller
 public class MainController {
+	
+	@Autowired
+	private BoardService boardService;
+	
+	public void setBoardService(BoardService boardService) {
+		this.boardService = boardService;
+	}
+	
 	@RequestMapping("/main")
-	public String main() {
+	public String main(Model model) {
+		List<BoardList> boardList = boardService.list();
+		model.addAttribute("boardList", boardList);
 		return "main";
 	}
 
