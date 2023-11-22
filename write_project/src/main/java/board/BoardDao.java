f13277fe7ad766bb947ecf8f0741f9d22b66a238
Chanmi An <bangoul345@gmail.com>
2023-11-22 16:25:56 +0900

    auto_increment 초기화 추가

--- write_project/src/main/java/board/BoardDao.java
+++ write_project/src/main/java/board/BoardDao.java
@@ -64,5 +64,8 @@ public class BoardDao {
 	
 	public void delete(int id) { //글 내용 삭제
 		jdbcTemplate.update("delete from board where id = ?", id);
+		jdbcTemplate.execute("alter table board auto_increment = 1");
+		jdbcTemplate.execute("set @count = 0");
+		jdbcTemplate.update("update board set id = @count:=@count+1");
 	}
 }
